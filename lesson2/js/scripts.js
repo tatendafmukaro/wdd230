d1f1844c667a4476b19e5db7cf1b71610f7b91dd
tatendafmukaro <tatendafmukaro@outlook.com>
2021-09-23 14:47:34 +0200

    week2

---
+++ lesson2/js/scripts.js
@@ -0,0 +1,7 @@
+//document.querySelector("copyrightyear").textContent = new Date().getFullYear;
+
+//document.getElementById("snowman").textContent = new Date().getFullYear;
+let mydate = new Date();
+let myyear = mydate.getFullYear();
+//document.getElementById("theyear").textContent = mydate;
+document.getElementById("lu").textContent = "Last Updated:" + document.lastModified ;
