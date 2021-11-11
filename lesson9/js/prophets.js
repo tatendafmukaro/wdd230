25571d9c3643043c04fdbac8349489800fb007b4
tatendafmukaro <tatendafmukaro@outlook.com>
2021-11-11 04:36:44 +0200

    prophetsupdate3

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -10,20 +10,20 @@ fetch(requestURL)
     for (let i = 0; i < prophets.length; i++ ) {
       let card = document.createElement('section');
       let h2 = document.createElement('h2');
-      let para1 = document.createElement('p'); // Updates
-      let para2 = document.createElement('p'); // Updates
+      let paragraph1 = document.createElement('p'); // Updates
+      let paragraph2 = document.createElement('p'); // Updates
       let image = document.createElement('img') // Updates
 
       h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-      para1.textContent = "Date of Birth:" + ' ' + prophets[i].birthdate; // Updates
-      para2.textContent = "Place of Birth:" + ' ' + prophets[i].birthplace; // Updates
+      paragraph1.textContent = "Date of Birth:" + ' ' + prophets[i].birthdate; // Updates
+      paragraph2.textContent = "Place of Birth:" + ' ' + prophets[i].birthplace; // Updates
       image.setAttribute('src', prophets[i].imageurl);
       image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + ' ' + "-" + ' ' + prophets[i].order); // Updates
         
 
       card.appendChild(h2);
-      card.appendChild(para1);
-      card.appendChild(para2);
+      card.appendChild(paragraph1);
+      card.appendChild(paragraph2);
       card.appendChild(image);
 
       document.querySelector('div.cards').appendChild(card);
