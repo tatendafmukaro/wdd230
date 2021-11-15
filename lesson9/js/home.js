f355af7b1d5758f9e3627fa134c41d49a7250e50
tatendafmukaro <tatendafmukaro@outlook.com>
2021-11-15 18:28:57 +0200

    wp-homepageupdates-linkupdates2

---
+++ lesson9/js/home.js
@@ -0,0 +1,54 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+     // console.table(jsonObject);
+      const towns = jsonObject['towns'];
+
+      const threeTowns = towns.filter((towns) => towns.name === 'Soda Springs' || towns.name === 'Fish Haven' || towns.name === 'Preston');
+      
+      threeTowns.sort(function(a, b) {
+        return parseFloat(b.currentPopulation) - parseFloat(a.currentPopulation);
+    });
+
+
+      threeTowns.forEach((xtown) => {
+
+        let townCard = document.createElement('section');
+        let div = document.createElement('div');
+        let image = document.createElement('img');
+        let name = document.createElement('h3');
+        let motto = document.createElement('h4');
+        let year = document.createElement('p');
+        let pop = document.createElement('p');
+        let rain = document.createElement('p');
+
+
+
+        image.setAttribute('src', `lesson9/images/towns/${xtown.photo}`);
+        
+       
+        image.setAttribute('alt', `nature photo in ${xtown.name}`);
+        div.className = 'town-info';
+        name.textContent = `${xtown.name}`;
+        motto.textContent = `${xtown.motto}`;
+        year.textContent = `Year Founded: ${xtown.yearFounded}`;
+        pop.textContent = `Population: ${xtown.currentPopulation}`;
+        rain.textContent = `Annual Rain Fall ${xtown.averageRainfall}`;
+
+        townCard.appendChild(image);
+        townCard.appendChild(div);
+    
+        div.appendChild(name);
+        div.appendChild(motto);
+        div.appendChild(year);
+        div.appendChild(pop);
+        div.appendChild(rain);
+        
+        document.querySelector('div.town-data').appendChild(townCard);
+        
+      });
+  });
--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -1,3 +1,4 @@
+// const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 
 fetch(requestURL)
