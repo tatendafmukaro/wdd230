3e16af6ec9ff67cc7cfa6ae738e656904a87b0b1
tatendafmukaro <tatendafmukaro@outlook.com>
2021-11-20 07:19:19 +0200

    wr-updates-gallery2

--- lesson7/js/friday-banner.js
+++ lesson7/js/friday-banner.js
@@ -1,11 +1,19 @@
-const pancakeAnnouncement = document.querySelector('.friday-banner');
+// const pancakeAnnouncement = document.querySelector('.friday-banner');
 
-const dayOfWeek= todaysDate.getDay();
+// const dayOfWeek= todaysDate.getDay();
 
-if ( dayOfWeek == 3 ) {
-  pancakeAnnouncement.style.display = "block";
-}
+// if ( dayOfWeek == 3 ) {
+//   pancakeAnnouncement.style.display = "block";
+// }
 
-pancakeAnnouncement.onclick = function(e) {
-  pancakeAnnouncement.style.display = "none";
+// pancakeAnnouncement.onclick = function(e) {
+//   pancakeAnnouncement.style.display = "none";
+// }
+
+let day = new Date().getDay();
+
+function setDiv() {
+    if (day == 5) {
+        document.getElementById("friday-banner").style.display = "block";
+ }
 }
--- lesson7/js/scripts.js
+++ lesson7/js/scripts.js
@@ -7,7 +7,7 @@
 
 let dayNames = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]
-let d =new Date();
+let d = new Date();
 let dayName = dayNames[d.getDay()];
 let monthName = months[d.getMonth()];
 let fullDate = dayName + ", " + d.getDate() + " " + monthName + " "  + d.getFullYear();
